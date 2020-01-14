942f84f03a81b524b9a3122d5f3b20d5148f50f2
Islam Zaripov <islam.zar@yandex.ru>
2020-01-14 07:26:07 +0300

    First

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+// css
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/app.css';
+
+// js
+//import $ from 'jquery';
+import 'bootstrap';
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+    el: '#app',
+    components: { App },
+})
---
+++ assets/js/component/Card.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            Simple card
+        </div>
+        <div class="card-body">
+            <h5 class="card-title mb-2">{{ title }}</h5>
+            <p class="card-text bg-dark text-white p-2 h6 mb-3">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary float-right">Next</a>
+        </div>
+        <div>
+            <ul>
+                <li v-for="(car, i) in cars" class="mb-1">
+                    {{car.name}}
+                    <span class="py-1 px-2 rounded-pill text-white bg-success">{{car.price}} $</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            title: { default: 'Title' },
+            cars: { default: [] }
+        },
+        data: function () {
+            return {
+                hover: false
+            }
+        }
+    };
+</script>
+
+<style scoped>
+    li {
+        list-style-type: circle;
+    }
+</style>
